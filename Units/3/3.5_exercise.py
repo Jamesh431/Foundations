0d513bcd14b02b4593486ef635507dc3bc4ebf63
James Hales <jdhales2686@gmail.com>
2023-08-06 19:04:35 -0600

    first commit

---
+++ Units/3/3.5_exercise.py
@@ -0,0 +1,18 @@
+first_name = 'James'
+last_name = 'hales'
+my_hobby = 'play guitar'
+my_age = 21
+pineapple_belongs_on_pizza = True
+
+print(first_name)
+
+print(my_age)
+
+print(pineapple_belongs_on_pizza)
+
+print("Hello! My name is" + " " + first_name + " " + last_name + ". I am " + str(my_age) + " years old." + "and I like to " + my_hobby + ".")
+# remember python can only concatenate str, not int, to a string. So use str
+
+combined_string = first_name + " " + last_name
+
+print(combined_string)
